c058165dcf08678a08bd3bd99a319c1267285be5
Roy <me@hiyorix.com>
2022-12-26 19:09:24 +0200

    Adding javadoc

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -4,17 +4,40 @@ public class ConcreteMember implements Member{
     private UndoableStringBuilder currentString;
     private String name;
 
+    /**
+     *
+     * A constructor with the default StringBuilder values.
+     *
+     * @param name  Name of the member.
+     *
+     */
     public ConcreteMember(String name) {
         this.name = name;
         this.currentString = null;
     }
 
+    /**
+     *
+     * Updates the pointer of the UndoableStringBuilder to
+     * the one from the GroupAdmin.
+     *
+     * @param usb  A pointer to the UndoableStringBuilder object
+     *             from the GroupAdmin.
+     *
+     */
     @Override
     public void update(UndoableStringBuilder usb) {
         this.currentString = usb;
         System.out.println(this.name + " notified.");
     }
 
+    /**
+     *
+     * A toString method to print the member's data.
+     *
+     * @return  A string representation of the object.
+     *
+     */
     public String toString() {
         return "Name: " + this.name + "; String: " + this.currentString;
     }
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -24,35 +24,84 @@ public class GroupAdmin implements Sender{
         }
     }
 
+    /**
+     *
+     * Inserts the string into this character sequence.
+     * Also notifies that a change has been made.
+     *
+     * @param offset  the offset
+     * @param str  a string
+     *
+     */
     @Override
     public void insert(int offset, String obj) {
         this.usb.insert(offset, obj);
         notifyMembers();
     }
 
+    /**
+     *
+     * Appends the specified string to this character sequence.
+     * Also notifies that a change has been made.
+     *
+     * @param obj  a string
+     *
+     */
     @Override
     public void append(String obj) {
         this.usb.append(obj);
         notifyMembers();
     }
 
+    /**
+     *
+     * Removes the characters in a substring of this sequence. The substring begins
+     * at the specified start and extends to the character at index
+     * end - 1 or to the end of the sequence if no such character exists.
+     * If start is equal to end, no changes are made.
+     * Also notifies that a change has been made.
+     *
+     * @param start  The beginning index, inclusive.
+     * @param end  The ending index, exclusive.
+     *
+     */
     @Override
     public void delete(int start, int end) {
         this.usb.delete(start, end);
         notifyMembers();
     }
 
+    /**
+     *
+     * Makes an undo command - replaces the current string with
+     * the previous string that was stored in memory.
+     * Also notifies that a change has been made.
+     *
+     */
     @Override
     public void undo() {
         this.usb.undo();
         notifyMembers();
     }
 
+    /**
+     *
+     * Notifies all members that a change to the UndoableStringBuilder
+     * object has been made.
+     *
+     */
     private void notifyMembers() {
         for (Member m: this.membersList)
             m.update(this.usb);
     }
 
+    /**
+     *
+     * A toString method to print the sender's data.
+     *
+     * @return  A string representation of the object.
+     *
+     */
     @Override
     public String toString() {
         String ret = "Members:\n";
